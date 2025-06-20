a6cbed4d5bb7788621935562b799fb5ab87fe31d
GiovaniBertoni <143568501+GiovaniBertoni@users.noreply.github.com>
2025-06-20 15:07:04 -0300

    Esqueleto do Flutter

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\User\\Desktop\\Hackaton\\corretor_gabaritos(front)" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\User\\Desktop\\Hackaton\\corretor_gabaritos(front)"
+  "FLUTTER_ROOT=C:\\Dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\User\\Desktop\\Hackaton\\corretor_gabaritos(front)\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\User\\Desktop\\Hackaton\\corretor_gabaritos(front)"
+  "FLUTTER_TARGET=C:\\Users\\User\\Desktop\\Hackaton\\corretor_gabaritos(front)\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\User\\Desktop\\Hackaton\\corretor_gabaritos(front)\\.dart_tool\\package_config.json"
+)
